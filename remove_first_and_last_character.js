4e045cc8f89c9597a34da4ee6352901272ed8ed9
Crew Spence <csspence123@gmail.com>
2020-01-31 06:18:33 -0600

    Wrote working solution for remove_first_and_last_character.

--- remove_first_and_last_character.js
+++ remove_first_and_last_character.js
@@ -4,7 +4,13 @@ You're given one parameter,
 the original string. You don't have to worry with strings with less than two characters.
 */
 
-function removeChar(str){
- //You got this!
+const removeChar = (str) => {
+ let newStr = '';
+ for(let i = 0; i < str.length; i++) {
+   if(i !== 0 && i !== str.length - 1) {
+     newStr += str[i];
+   }
+ }
 
-};
+ return newStr;
+}
